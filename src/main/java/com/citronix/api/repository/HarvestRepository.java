5590f46f61b857a82c6dc4144d9ed4b3110346b1
BilalChbanat <bilal.chbanat2003@gmail.com>
2024-11-24 18:14:22 +0100

    implement Harvest Repository

---
+++ src/main/java/com/citronix/api/repository/HarvestRepository.java
@@ -0,0 +1,9 @@
+package com.citronix.api.repository;
+
+import com.citronix.api.domains.Harvest;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HarvestRepository extends JpaRepository<Harvest, Long> {
+
+
+}
